8c7d62aeb61a320ad646f3a36034d78db78947e6
santoshpavan <santoshpavan.psp@gmail.com>
2020-06-22 18:45:21 -0400

    burger ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>
             Toolbar, SideDrawer and Backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
