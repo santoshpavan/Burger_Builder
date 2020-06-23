e96eb9b4f077f1667d950b7ae7dd7bb5e00e82b9
santoshpavan <santoshpavan.psp@gmail.com>
2020-06-22 23:01:39 -0400

    dynamically adding ingredients using state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    const mid_ingredients = Object.keys(props.ingredients)
+    .map((ingredient_key) => {
+            return ([...Array(props.ingredients[ingredient_key])]
+            .map((_, i) => {
+                    return (<BurgerIngredient key={ingredient_key + i} type={ingredient_key} />);
+            }))
+    });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {mid_ingredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
