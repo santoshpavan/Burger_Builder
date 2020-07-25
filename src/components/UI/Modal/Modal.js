719409e6bb8b1233d8cac50e5982f7260f6f7e6d
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-25 19:21:58 -0400

    Order summary (through Modal) modified for better performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return(
-        <Aux>
-            <Backdrop show={props.show} backdrop_clicked={props.backdrop_clicked}/>
-            <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-            }}>
-                {props.children}
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+    // this should update only when the "show" changes
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render () {
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} backdrop_clicked={this.props.backdrop_clicked}/>
+                <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
 }
 
-export default modal;
+export default Modal;
