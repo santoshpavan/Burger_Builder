df1e6d116a37a67128c51daca2002c74fd49079b
santoshpavan <santoshpavan.psp@gmail.com>
2020-08-07 21:36:28 -0400

    Form validations

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,10 @@ import classes from './Button.css';
 
 const button = (props) => {
     return(
-        <button className={[classes.Button, classes[props.type]].join(" ")} onClick={props.clicked}>
-            {props.children}
-        </button>
+        <button 
+            disabled={props.disabled}
+            className={[classes.Button, classes[props.type]].join(" ")} 
+            onClick={props.clicked}>{props.children}</button>
     );
 }
 
