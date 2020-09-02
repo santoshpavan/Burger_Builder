494d5c17b15dd215f1fb69d5eccffe646b714ea2
santoshpavan <santoshpavan.psp@gmail.com>
2020-09-02 15:38:55 -0400

    authentication basic fields added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigation_items = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
+        <NavigationItem link="/auth">Authenticate</NavigationItem>
     </ul>
 );
 
