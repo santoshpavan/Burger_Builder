bc00deef0b9daeab209e4fa447e560b9ba60be8d
santoshpavan <santoshpavan.psp@gmail.com>
2020-12-16 02:04:00 -0500

    correcting links

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import fireBaseKey from '../../constants';
 
 import * as actionTypes from './actionTypes';
 
@@ -48,9 +49,10 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+        let url = baseUrl + fireBaseKey;
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = baseUrl + fireBaseKey;
         }
         axios.post(url, authData)
             .then(response => {
